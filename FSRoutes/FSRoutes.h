cd9a6be2137c539fd51b150a814f25604c236136
qfu <fuqiang@zhihu.com>
2017-09-20 01:47:59 +0800

    some init code

--- FSRoutes/FSRoutes.h
+++ FSRoutes/FSRoutes.h
@@ -7,6 +7,14 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FSRoutes : NSObject
 
+- (instancetype)init;
+- (BOOL)routeURL:(NSURL *)URL;
+- (BOOL)routeURL:(NSURL *)URL parameters:(nullable NSDictionary<NSString *, id> *)parameters;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- FSRoutes/FSRoutes.m
+++ FSRoutes/FSRoutes.m
@@ -7,6 +7,29 @@
 
 #import "FSRoutes.h"
 
+@interface FSRoutes()
+
+@property (nonatomic, strong) NSMutableOrderedSet *rules;
+
+@end
+
 @implementation FSRoutes
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _rules = [NSMutableOrderedSet orderedSet];
+    }
+    return self;
+}
+
+- (BOOL)routeURL:(NSURL *)URL {
+    return [self routeURL:URL parameters:nil];
+}
+
+- (BOOL)routeURL:(NSURL *)URL parameters:(nullable NSDictionary<NSString *, id> *)parameters {
+    NSParameterAssert(URL);
+    return NO;
+}
 
 @end
