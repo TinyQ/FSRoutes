8254065d5d0b15e3bdbe2e8c69f8d3a1d84ebf35
qfu <fuqiang@zhihu.com>
2017-09-21 01:38:33 +0800

    commit

--- FSRoutes/FSRoutesMatcher.m
+++ FSRoutes/FSRoutesMatcher.m
@@ -10,6 +10,8 @@
 @interface FSRoutesMatcher()
 
 @property (nonatomic) NSString *rule;
+@property (nonatomic) NSString *rule_scheme;
+@property (nonatomic) NSString *rule_expression;
 
 @end
 
@@ -24,12 +26,35 @@
     self = [super init];
     if (self) {
         _rule = rule;
+        _rule_scheme = [self schemeFromRule:rule];
+        _rule_expression = [self expressionFromRule:rule];
     }
     return self;
 }
 
 - (FSRoutesMatchResult *)match:(NSURL *)url {
+    NSParameterAssert(url);
+    NSString *url_scheme = url.scheme;
+    if (!self.rule_scheme) {
+        return nil;
+    }
+    if (![self.rule_scheme isEqualToString:url_scheme]) {
+        return nil;
+    }
+    // TODO:
     return nil;
+}
+
+#pragma mark - private
+
+- (NSString *)schemeFromRule:(NSString *)rule {
+    NSArray *parts = [rule componentsSeparatedByString:@"://"];
+    return parts.count > 1 ? [parts firstObject] : nil;
+}
+
+- (NSString *)expressionFromRule:(NSString *)rule {
+    NSArray *parts = [rule componentsSeparatedByString:@"://"];
+    return parts.count == 2 ? [parts lastObject] : nil;
 }
 
 @end
