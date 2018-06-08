9e4ec5818677358164351a0e390c7f46c3b96e29
qfu <fuqiang@zhihu.com>
2018-06-09 00:34:26 +0800

    update test case

--- FSRoutesProject/FSRoutesProjectTests/FSRoutesMatcherTests.m
+++ FSRoutesProject/FSRoutesProjectTests/FSRoutesMatcherTests.m
@@ -76,11 +76,80 @@ NSURL *URLWithPath(NSString *path) {
     matcher = [FSRoutesMatcher matcherWithRule:@"abc://def"];
     expect([matcher ruleExpression]).to.equal(@"def");
     
+    matcher = [FSRoutesMatcher matcherWithRule:@"abc://def/ghi"];
+    expect([matcher ruleExpression]).to.equal(@"def/ghi");
+    
     matcher = [FSRoutesMatcher matcherWithRule:@"/def/abc"];
     expect([matcher ruleExpression]).to.equal(@"/def/abc");
 }
 
-- (void)test_ruleParameter_0 {
+- (void)test_when_a_host_match {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"qfu.space"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(YES);
+    expect(result.parameter).to.equal(@{});
+}
+
+- (void)test_when_a_host_not_match {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"qfu.me"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(NO);
+    expect(result.parameter).to.equal(nil);
+}
+
+- (void)test_when_a_host_match_and_path_match {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"qfu.space/abc/def"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"/abc/def");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(YES);
+    expect(result.parameter).to.equal(@{});
+}
+
+- (void)test_when_a_host_not_match_and_path_match {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"qfu.me/abc/:id"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"/abc/123");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(NO);
+    expect(result.parameter).to.equal(nil);
+}
+
+- (void)test_when_a_host_match_and_path_not_match {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"qfu.space/abc/:id"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"/def/123");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(NO);
+    expect(result.parameter).to.equal(nil);
+}
+
+- (void)test_when_a_host_not_match_and_path_not_match {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"qfu.me/abc/:id"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"/def/123");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(NO);
+    expect(result.parameter).to.equal(nil);
+}
+
+- (void)test_when_a_url_matches_a_route {
     FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"/def"];
     expect(matcher).notTo.beNil();
     
@@ -91,7 +160,7 @@ NSURL *URLWithPath(NSString *path) {
     expect(result.parameter).to.equal(@{});
 }
     
-- (void)test_ruleParameter_1 {
+- (void)test_when_a_url_matches_a_route_with_1_parameter {
     FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"/def/:id"];
     expect(matcher).notTo.beNil();
 
@@ -102,7 +171,7 @@ NSURL *URLWithPath(NSString *path) {
     expect(result.parameter[@"id"]).to.equal(@"123");
 }
     
-- (void)test_ruleParameter_2 {
+- (void)test_when_a_url_matches_a_route_with_2_parameter_style_1 {
     FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"/def/:id/:name"];
     expect(matcher).notTo.beNil();
     
@@ -112,6 +181,30 @@ NSURL *URLWithPath(NSString *path) {
     expect(result.match).to.equal(YES);
     expect(result.parameter[@"id"]).to.equal(@"123");
     expect(result.parameter[@"name"]).to.equal(@"456");
+}
+
+- (void)test_when_a_url_matches_a_route_with_2_parameter_style_2 {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"/id/:id/name/:name"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"/id/123/name/qfu");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(YES);
+    expect(result.parameter[@"id"]).to.equal(@"123");
+    expect(result.parameter[@"name"]).to.equal(@"qfu");
+}
+
+- (void)test_when_a_url_matches_a_route_with_2_parameter_style_3 {
+    FSRoutesMatcher *matcher = [FSRoutesMatcher matcherWithRule:@"/:id/name/:name"];
+    expect(matcher).notTo.beNil();
+    
+    NSURL *url = URLWithPath(@"/123/name/qfu");
+    FSRoutesMatchResult *result = [matcher match:url];
+    expect(result).notTo.beNil();
+    expect(result.match).to.equal(YES);
+    expect(result.parameter[@"id"]).to.equal(@"123");
+    expect(result.parameter[@"name"]).to.equal(@"qfu");
 }
 
 @end
