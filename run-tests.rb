3800b467780ffbb772b788be5858113e2a2f7430
qfu <fuqiang@zhihu.com>
2018-06-10 00:06:06 +0800

    fix run-tests.rb

--- run-tests.rb
+++ run-tests.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/ruby
 
+require 'fileutils'
+def RAISE_IF_SYSTEM_FAILED(command)
+    system command or \
+        raise("\n\n ❌  command run failed : #{command} \n\n")
+end
+
+def RUN_SHELL_IN_PATH(command,path)
+    path_current = Dir.pwd
+    FileUtils.cd(path)
+    RAISE_IF_SYSTEM_FAILED(command)
+    FileUtils.cd(path_current)
+end
+
 workspace = "FSRoutesProject/FSRoutesProject.xcworkspace"
 scheme = "FSRoutesProject"
 sdk = `xcodebuild -showsdks | grep -o 'iphonesimulator*.*'`
-destination = "'platform=iOS Simulator,name=iPhone X,OS=#{sdk[15..18]}'"
+destination = "'platform=iOS Simulator,name=iPhone 7,OS=10.0'"
 
 xcodebuild_test_command = <<-EOF
 xcodebuild\
@@ -14,4 +27,5 @@ xcodebuild\
  test | xcpretty --test --color && exit ${PIPESTATUS[0]}
 EOF
 
-system xcodebuild_test_command
+RUN_SHELL_IN_PATH(xcodebuild_test_command ,'.')
+
